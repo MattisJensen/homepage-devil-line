540dedb79be4d08a9396c4b4a508861730e7bd63
Mattis <matjen@mail.de>
2025-08-20 00:24:16 +0200

    gh pages fix 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
     site: "https://mattisjensen.github.io/homepage-devil-line",
-    base: "/homepage-devil-line",
+    // base: "/",
     // trailingSlash: 'always',
     integrations: [
         alpinejs(),
